f4b60b3f5c36f082b59bd892e64afba31d2d0841
Iosif.Barta <iosif.barta@gmail.com>
2020-06-22 20:10:47 +0300

    added integration tests for creating product

---
+++ src/test/java/org/fasttrackit/onlineshop/ProductServiceIntegrationTests.java
@@ -0,0 +1,53 @@
+package org.fasttrackit.onlineshop;
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+@SpringBootTest
+class ProductServiceIntegrationTests {
+
+    //field injection
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    void createProduct_whenValidRequest_thenReturnCreatedProduct() {
+        SaveProductRequest request = new SaveProductRequest();
+        request.setName("Phone");
+        request.setPrice(500);
+        request.setQuantity(1000);
+
+        Product product = productService.createProduct(request);
+
+        //assertions
+        assertThat(product, notNullValue());
+        assertThat(product.getId(), greaterThan(0L));
+        assertThat(product.getName(), is(request.getName()));
+        assertThat(product.getPrice(), is(request.getPrice()));
+        assertThat(product.getQuantity(), is(request.getQuantity()));
+
+    }
+
+    void createProduct_whenMissingMandatoryProperties_theThrowException(){
+         SaveProductRequest request = new SaveProductRequest();
+
+
+        try {
+            productService.createProduct(request);
+        } catch (Exception e) {
+            assertThat("Unexpected exception throw", e instanceof ConstraintViolationException);
+
+        }
+    }
+}
